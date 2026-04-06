4d50ceb1cd6ea1ad95a784de8019ba31925d9f24
Clirim Krasniqi <ckrasniq@3-F-4.42heilbronn.de>
2026-04-06 14:07:27 +0200

    Added the helper functions for the Character

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -1,10 +1,15 @@
 #include "Character.hpp"
 
-Character::Character() : ICharacter(){
+Character::Character() :
+ICharacter(),
+_name("Default")
+{
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
 	std::cout << "Character's default constructor called" << std::endl;
 }
 
-Character::Character ( const Character &other ) : ICharacter(other){
+Character::Character ( const Character &other ) : ICharacter(other), _name(""){
 	*this = other;
 	std::cout << "Character's copy constructor called" << std::endl;
 }
@@ -15,6 +20,26 @@ Character &Character::operator=(const Character &other){
 	}
 	std::cout << "Character's copy assignment opertaor called" << std::endl;
 	return *this;
+}
+
+const std::string &Character::getName(){
+	return this->_name;
+}
+
+void Character::equip(AMateria *m){
+	for (int i = 0; i < 4; i++){
+		if (this->_inventory[i] == NULL){
+			this->_inventory[i] = m;
+			return;
+		}
+	}
+}
+
+void Character::unequip(int idx){
+}
+
+void Character::use(int idxx, ICharacter &target){
+
 }
 
 Character::~Character(){
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -2,16 +2,21 @@
 #define CHARACTER_HPP
 
 #include "ICharacter.hpp"
-#include "AMateria.hpp"
+
 
 class Character : public ICharacter{
 	public:
 		Character();
 		Character( const Character &other );
 		Character &operator=( const Character &other );
+		std::string const &getName();
+		void equip(AMateria* m);
+		void unequip(int idx);
+		void use(int idxx, ICharacter& target);
 
 		~Character();
 	protected:
+		std::string _name;
 		AMateria* _inventory[4];
 };
 
