a160c0e81737ded928631770d04489457f6eeef3
Clirim Krasniqi <ckrasniq@3-F-2.42heilbronn.de>
2026-04-08 13:54:29 +0200

    Now handling the inventory correctly and deleting everything before we clone

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -5,9 +5,17 @@ Character::Character() : ICharacter(), _name("Default"){
 		_inventory[i] = NULL;
 	std::cout << "Character's default constructor called" << std::endl;
 }
-Character::Character( std::string const &name ): _name(name){
+
+Character::Character(const std::string &name) : _name(name){
 	for (int i = 0; i < 4; i++)
 		_inventory[i] = NULL;
+	std::cout << "Character " << _name << " created" << std::endl;
+}
+
+Character::Character(const Character &other) : _name(other._name) {
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
+	*this = other;
 	std::cout << "Character's copy constructor called" << std::endl;
 }
 
@@ -15,9 +23,10 @@ Character &Character::operator=(const Character &other){
 	if (this != &other){
 		this->_name = other._name;
 		for (int i = 0; i < 4; i++){
-			if (this->_inventory[i] != NULL)
+			if (this->_inventory[i] != NULL){
 				delete this->_inventory[i];
-
+				_inventory[i] = NULL;
+			}
 			if (other._inventory[i] != NULL)
 				this->_inventory[i] = other._inventory[i]->clone();
 			else
@@ -28,11 +37,12 @@ Character &Character::operator=(const Character &other){
 	return *this;
 }
 
-const std::string &Character::getName(){
+std::string const &Character::getName() const{
 	return this->_name;
 }
 
 void Character::equip(AMateria *m){
+	if (!m) return;
 	for (int i = 0; i < 4; i++){
 		if (this->_inventory[i] == NULL){
 			this->_inventory[i] = m;
@@ -42,18 +52,20 @@ void Character::equip(AMateria *m){
 }
 
 void Character::unequip(int idx){
-	if (idx < 0 || idx > 4 || !this->_inventory[idx])
+	if (idx < 0 || idx >= 4 || !this->_inventory[idx])
 		return;
 	this->_inventory[idx] = NULL;
 }
 
 void Character::use(int idxx, ICharacter &target){
-	if (idxx < 0 || idxx > 4 || !this->_inventory[idxx])
+	if (idxx < 0 || idxx >= 4 || !this->_inventory[idxx])
 		return;
 	this->_inventory[idxx]->use(target);
-
 }
 
 Character::~Character(){
+	for (int i = 0; i < 4; i++){
+		if (this->_inventory[i]) delete this->_inventory[i];
+	}
 	std::cout << "Character's destructor called!" << std::endl;
 }
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -1,14 +1,16 @@
 #ifndef CHARACTER_HPP
 #define CHARACTER_HPP
 
+#include "AMateria.hpp"
 #include "ICharacter.hpp"
 
 class Character : public ICharacter{
 	public:
 		Character();
-		Character( std::string const &name );
+		Character( std::string const & name );
+		Character( const Character &other );
 		Character &operator=( const Character &other );
-		std::string const &getName();
+		std::string const &getName() const;
 		void equip(AMateria* m);
 		void unequip(int idx);
 		void use(int idxx, ICharacter& target);
