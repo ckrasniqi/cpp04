3cb64b5b498ae61011fc93e4997715f25c149f8b
Clirim Krasniqi <ckrasniq@3-F-2.42heilbronn.de>
2026-04-08 13:55:45 +0200

    Had to follow the subject since we have to deelte the materias and then clone them

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -1,17 +1,29 @@
 #include "MateriaSource.hpp"
 
 MateriaSource::MateriaSource() : IMateriaSource(){
+	for (int i = 0; i < 4; i++)
+		_materias[i] = NULL;
 	std::cout << "MateriaSource's default constructor called" << std::endl;
 }
 
 MateriaSource::MateriaSource ( const MateriaSource &other ) : IMateriaSource(other){
+	for (int i = 0; i < 4; i++)
+		_materias[i] = NULL;
 	*this = other;
 	std::cout << "MateriaSource's copy constructor called" << std::endl;
 }
 
 MateriaSource &MateriaSource::operator=(const MateriaSource &other){
 	if (this != &other){
-		IMateriaSource::operator=(other);
+		for (int i = 0; i < 4; i++){
+			if (this->_materias[i] != NULL)
+				delete this->_materias[i];
+
+			if (other._materias[i] != NULL)
+				this->_materias[i] = other._materias[i]->clone();
+			else
+				this->_materias[i] = NULL;
+		}
 	}
 	std::cout << "MateriaSource's copy assignment opertaor called" << std::endl;
 	return *this;
@@ -36,7 +48,10 @@ AMateria *MateriaSource::createMateria(std::string const & type){
 	return 0;
 }
 
-
 MateriaSource::~MateriaSource(){
+	for (int i = 0; i < 4; i++){
+		if (this->_materias[i])
+			delete this->_materias[i];
+	}
 	std::cout << "MateriaSource's destructor called!" << std::endl;
 }
